ade4db23f28148bc7dfb379f43f977840a9970ea
utility4u <benedikthofirek@gmail.com>
2021-04-02 20:41:35 +0200

    initial commit

---
+++ app/src/main/java/com/hofirek/todoapp/Todo.kt
@@ -0,0 +1,11 @@
+package com.hofirek.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Todo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo)
+    }
+}
